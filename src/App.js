dc5fde28c8ea60196d2ce8e613acc9ba848256eb
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2021-07-20 15:18:58 +0530

    day 5 done.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">hi</div>;
+  const [timer, setTimer] = useState(0);
+  const intervalReference = useRef();
+  useEffect(() => {
+    intervalReference.current = setInterval(() => {
+      setTimer((prevTimer) => prevTimer + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalReference.current);
+    };
+  });
+  return (
+    <div className="App">
+      <h1>Timer - {timer}</h1>
+      <button onClick={() => clearInterval(intervalReference.current)}>
+        Clear interval
+      </button>
+    </div>
+  );
 }
 
 export default App;
